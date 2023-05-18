c278525dd5ef2f777c7f17109f14cec038bbeced
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-18 15:13:22 -0300

    Aula 43 - Ajustes de configuração inicial do projeto.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'gerenciador-de-tarefas'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Gerenciador de tarefas');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Gerenciador de tarefas app is running!');
-  });
 });
