8d8a004aee759db19c7cd2c907142aa8f26e4b3b
Eduardo Comin <eduardo.comin@capgemini.com>
2023-05-23 10:52:20 -0300

    Aula 72 - Ajustando testes unitários.

--- src/app/tarefas/shared/tarefa-concluida.directive.spec.ts
+++ src/app/tarefas/shared/tarefa-concluida.directive.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef } from '@angular/core';
 import { TarefaConcluidaDirective } from './tarefa-concluida.directive';
 
 describe('TarefaConcluidaDirective', () => {
+  let el: ElementRef;
   it('should create an instance', () => {
-    const directive = new TarefaConcluidaDirective();
+    const directive = new TarefaConcluidaDirective(el);
     expect(directive).toBeTruthy();
   });
 });
